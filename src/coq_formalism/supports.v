6ffc31da9d812b26f8635d9523a6a9eb2b2cdac0
Anna Fritz <arfritzz@ku.edu>
2023-09-26 15:40:07 -0500

    new definition of supports

---
+++ src/coq_formalism/supports.v
@@ -0,0 +1,89 @@
+(**** Labeled Graph Homomorphism 
+      By: Anna Fritz 
+      Date: July 18, 2023 
+      
+      Defining supports and covers and other interesting 
+      properties taken from Paul Rowe's paper: 
+      "On Orderings in Security Models" *)
+
+Require Import Coq.Lists.List.
+
+Require Import Order.attack_graph.
+Require Import Order.strict_partial_order.
+Require Import Order.reduce.
+Require Import Order.equiv.
+
+Require Import Coq.Sets.Ensembles.
+
+(********** 
+    SUPPORTS 
+   
+    CHASE analysis of a Copland Protocol generates 
+    a set of graphs. We want to be able to compare 
+    these sets of graphs so we introduce the idea 
+    of supports as motivated by Rowe's paper.
+    *********)
+
+Section Supports. 
+
+Print homomorphism.
+
+Context {measurement : Type}.
+Context {corruption : Type}.
+
+(* Given two sets of graphs SS and TT, we say
+ * that SS supports TT iff for every H in TT
+ * there exists some G in SS such that 
+ * G < H *)
+Definition supports (SS : Ensemble (attackgraph measurement corruption)) (TT : Ensemble (attackgraph measurement corruption)) : Prop := 
+  forall (H : (attackgraph measurement corruption)), In _ TT H ->
+  ( exists (G : (attackgraph measurement corruption)), In _ SS G /\ strict_partial_order (G.(steps _ _)) (H.(steps _ _))).
+  
+(* TODO *)
+Theorem supports_irr : forall SS, ~ supports SS SS.
+Proof.
+    intros. unfold supports. 
+    unfold not. intros. 
+Abort. 
+
+Theorem supports_trans : forall SS TT PP, supports SS TT -> supports TT PP -> supports SS PP.
+Proof.
+    unfold supports. intros SS TT PP Hst Htp h' Hh'.
+    specialize Htp with h'.
+    destruct Htp; eauto.
+    specialize Hst with x.
+    destruct Hst. destruct H; eauto.
+    destruct H. destruct H0.
+    exists x0; split; eauto.
+    eapply spo_trans; eauto.
+Qed. 
+ 
+
+(* Traditional def of supports. This generates a preorder on attack graphs *)
+Definition supports_trad (SS : Ensemble (attackgraph measurement corruption)) (TT : Ensemble (attackgraph measurement corruption)) : Prop := 
+    forall (H : (attackgraph measurement corruption)), In _ TT H ->
+    ( exists (G : (attackgraph measurement corruption)), In _ SS G /\ exists f, homomorphism G H f ).
+  
+(* Proving traditional definition of supports is reflexive and transitive 
+ * (ie a preorder) *)
+
+Theorem supports_trad_refl : forall SS, supports_trad SS SS.
+Proof.
+  unfold supports_trad. intros. exists H; split; eauto. apply homomorphism_refl.
+Qed.
+
+Theorem supports_trad_trans : forall SS TT PP, supports_trad SS TT -> supports_trad TT PP -> supports_trad SS PP.
+Proof.
+   unfold supports_trad. intros SS TT PP Hst Htp h' Hh'.
+   specialize Htp with h'.
+   destruct Htp; eauto.
+   specialize Hst with x.
+   destruct Hst. destruct H; eauto.
+   destruct H. destruct H0.
+   exists x0; split; eauto.
+   eapply homomorphism_trans; eauto.
+Qed. 
+
+End Supports. 
+
+(* TODOs:  Prove supports is a strict partial order  *)
