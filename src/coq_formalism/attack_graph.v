a0e92ffffa86b12a69a163baad328f264af9bd82
Anna Fritz <arfritzz@ku.edu>
2023-10-05 09:44:49 -0500

    get_steps

--- src/coq_formalism/attack_graph.v
+++ src/coq_formalism/attack_graph.v
@@ -7,3 +7,9 @@ Record attackgraph (measurement corruption : Type) : Type :=
     steps : list (state * state) ;
     label : state -> measurement + corruption
 }.
+
+
+Definition get_steps {meas cor : Type} (a : attackgraph meas cor) := a.(steps _ _).
+
+(* Assumptions: 
+ * - complete : every set of attack graphs is complete (has every possible graph) *)
