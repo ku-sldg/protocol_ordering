dcd8a4079e8b116a96b2a26b5bcd29d4e7f9060a
Sarah-Scott <eyeluvmp3s@gmail.com>
2024-03-27 19:00:34 -0500

    antisymmetry with new set reduction

--- src/coq_formalism/set_order.v
+++ src/coq_formalism/set_order.v
@@ -15,6 +15,7 @@ Require Import Order.utilities.
 Require Import Order.graph_partial_order.
 Require Import Order.supports_facts.
 Require Import Order.set_equiv.
+Require Import Order.set_reduce.
 
 Require Import Coq.Program.Equality.
 
@@ -130,199 +131,86 @@ Proof.
 Qed.   
 
 
-Theorem  supports_antisym : forall x y,
-supports x y -> supports y x -> set_eq x y.
+(******************************* 
+  SUPPORTS WITH REDUCE SET
+********************************)
+
+Theorem getallchains_supports_y : forall x y y',
+  getAllChains y y y' -> 
+  supports x y ->
+  supports x y'.
 Proof.
-  intros; unfold supports in *. simpl in *. induction x.
-  Abort.
+  intros x y y' YChains Sup. unfold supports in *.
+  intros H HIn'. eapply getallchains_in in HIn'; eauto.
+Qed.
 
-
-(* FUTURE WORK 
-   Not included in Fritz Dissertation work *)
-(*****************************
- graph_normalization SET TO EASIEST GRAPHS 
-*****************************)
-
-Inductive reduce_set (orig : list (attackgraph measurement adversary)) : list (attackgraph measurement adversary) ->  list (attackgraph measurement adversary) -> Prop :=
-| set_nil : reduce_set orig nil nil
-(* there does not exist anything that is less than a1 *)
-| set_keep : forall a1 SS TT, (forall a2, In a2 orig -> ~ strict_partial_order a2 a1) -> reduce_set orig SS TT -> reduce_set orig (a1 :: SS) (a1 :: TT) 
-(* set_remove case causes problems *)
-| set_remove : forall a1 SS TT, (exists a2, In a2 orig /\ strict_partial_order a2 a1) -> reduce_set orig SS TT -> reduce_set orig (a1 :: SS) TT. 
-
-Check reduce_set. 
-
-Theorem reduce_set_in : forall x x', 
-reduce_set x x x' ->
-forall a, In a x' ->
-In a x.
+Theorem getallchains_supports_x : forall x y x',
+  getAllChains x x x' ->
+  x <> nil ->
+  supports x y ->
+  supports x' y.
 Proof.
-intros. induction H;
-simpl in *; intuition.
+  intros x y x' HChains XCons Sup. 
+  unfold supports in *. intros H HInY.
+  pose proof HInY as HSup.
+  apply Sup in HSup. clear Sup.
+  destruct HSup as [G HSup]. destruct HSup as [GInX HLeq].
+  pose proof (getallchains_result x x' HChains G GInX) as HRes.
+  destruct HRes as [G' HRes]. destruct HRes as [G'InX' G'LeqG].
+  exists G'. split; auto. eapply po_trans; eauto.
 Qed.
 
 
-Theorem reduce_set_supports : forall x x',
-reduce_set x x x' ->
-supports x x'.
-Proof.
-intros x x' XRed. unfold supports.
-intros H HIn.
-induction XRed; subst.
-- inversion HIn.
-- destruct HIn; subst.
--- exists H. split; auto with *. left. apply iso_refl.
--- apply IHXRed in H1. destruct H1 as [G H1]. destruct H1.
- exists G. split; auto with *.
-- apply IHXRed in HIn. destruct HIn as [G H1]. destruct H1.
-exists G. split; auto with *.
-Qed.
+(******************************* 
+  SUPPORTS AS PARTIAL ORDER 
+********************************)
 
-Theorem reduce_set_supports_y : forall x y y',
-reduce_set y y y' ->
-supports x y ->
-supports x y'.
-Proof.
-intros x y y' YRed Sup. unfold supports in *.
-intros H HIn. eapply reduce_set_in in HIn; eauto.
-Qed.
-
-Theorem reduce_set_keep : forall orig x x',
-reduce_set orig x x' ->
-forall g', In g' x' ->
-forall g, In g orig ->
-~ strict_partial_order g g'.
-Proof.
-intros orig x x' XRed g' Inx' g Inorig contra.
-induction XRed.
-- inversion Inx'.
-- destruct Inx'.
--- subst. specialize H with g. intuition.
--- apply IHXRed. auto.
-- apply IHXRed. auto.
-Qed.
-
-(* if x reduces to x'  then 
- * forall g in x and not in x', 
- * there exists some g2 in the original 
- * that is strictly less than g *)
-Theorem reduce_set_remove : forall orig x x',
-reduce_set orig x x' ->
-forall g, In g x -> ~(In g x') ->
-exists g2, (In g2 orig /\ (strict_partial_order g2 g)).
-Proof.
-intros orig x x' XRed g Inx NInx'.
-induction XRed.
-- inversion Inx.
-- destruct Inx.
--- subst. exfalso. apply NInx'. auto with *.
--- apply IHXRed; auto. intros contra. apply NInx'. auto with *.
-- destruct Inx.
--- subst. assumption.
--- intuition.
-Qed.
-
-(* if the reduced set returns nil then the original set 
- * must've been nil *)
-Theorem reduce_set_nil : forall x,
-reduce_set x x nil ->
-x = nil.
-Proof.
-intros x XRed.
-destruct x.
-- reflexivity.
-- exfalso. apply supports_spo_irrefl with (a::x).
--- intros contra. inversion contra.
--- unfold supports_spo. intros.
-   eapply reduce_set_remove. eauto. eauto. eauto.
-Qed.
-
-Theorem reduce_set_lt : forall orig x x',
-  reduce_set orig x x' ->
-  forall g', In g' x' ->
-  forall g, In g orig ->
-  ~ strict_partial_order g g'.
-Proof.
-  intros orig x x' XRed g' Inx' g Inorig contra.
-  induction XRed.
-  - inversion Inx'.
-  - destruct Inx'.
-  -- subst. specialize H with g. intuition.
-  -- apply IHXRed. auto.
-  - apply IHXRed. auto.
-Qed.
-
-Theorem in_reduced_orig' : forall orig x xs xs', In x xs' -> reduce_set orig xs xs' -> In x xs.
-Proof.
-  intros. simpl in *. induction H0.
-  + invc H. 
-  + simpl in *. destruct H. eauto. eauto.
-  + simpl in *. right. eauto. 
-Qed.
-
-Lemma reduced_supports : forall orig x x' y, 
-  reduce_set orig x x' -> supports x' y -> supports x y.
-Proof.
-  intros. unfold supports in *. intros.
-  apply H0 in H2. destruct H2. destruct H2.
-  pose proof in_reduced_orig' orig x0 x x'.
-  exists x0;
-  intuition.
-Qed.
-
-Lemma reduced_supports_y_x_x' : forall x x' y, 
-  reduce_set x x x' -> supports y x -> supports y x'.
-Proof.
-  intros x x' y red. induction red.
-  + unfold supports. intros. invc H1.
-  + intros. unfold supports in *.
-    intros. destruct H2.
-  ++ subst. specialize H0 with H1. simpl in H0. intuition.
-  ++ eapply IHred; auto with *.
-  + intros. unfold supports in *.
-    intros. apply IHred; auto with *.
-Qed.          
-
-Ltac unfold_all := unfold supports in *; unfold supports_iso in *.
-
-Theorem reduce_set_remove' : forall orig x x',
-  reduce_set orig x x' ->
-  forall g, In g x -> ~(In g x') ->
-  exists g', (In g' x' /\ (strict_partial_order g' g)).
-Proof.
-Abort. 
-
-Lemma reduced_supports_x_x' : forall orig x x' y, 
-  reduce_set orig x x' -> supports x y -> supports x' y.
-Proof.
-  intros.
-  (* pose proof (reduce_set_keep orig x x') as keep.
-  pose proof (reduce_set_remove orig x x') as remove.*)
-  intuition.
-  unfold_all. intros.
-  induction H.
-  + apply H0 in H2. destruct H2. destruct H. invc H.
-  + intuition. pose proof (reduce_set_keep orig SS TT) as keep. intuition.
-Abort.    
-
-
-Theorem  supports_antisym' : forall x y x' y',
+(* supports is antisymmetric modulo set reduction *)
+Theorem  supports_antisym : forall x y x' y', 
+y <> nil -> x <> nil -> 
 supports x y -> supports y x -> 
-reduce_set x x x' -> reduce_set y y y' -> set_eq x' y'. 
+getAllChains x x x' -> getAllChains y y y' -> set_eq x' y'.
 Proof.
-intros X Y. intros X' Y'.
-intros (* YNil XNil *) supXY supYX.
-intros redX' redY'.
-unfold set_eq. split.
-(* X' is isomorphic to Y' *)   
-+ pose proof (reduced_supports_y_x_x' X X' Y). intuition. clear supYX.
-  pose proof (reduced_supports_y_x_x' Y Y' X). intuition. clear supXY.
-  pose proof (reduce_set_nil X).
-  destruct X.
-++ unfold_all. intros. admit.
-++ unfold_all. intros.  eapply H0 in H3. destruct H3. destruct H3.
-   induction redX'.   
-   Abort.
+  intros X Y. intros X' Y'.
+  intros YNil XNil supXY supYX.
+  intros chainsX' chainsY'.
+  unfold set_eq.
+  pose proof (getallchains_supports_x X Y X' chainsX' XNil supXY) as supX'Y.
+  pose proof (getallchains_supports_y X' Y Y' chainsY' supX'Y) as supX'Y'.
+  clear supXY supX'Y.
+  pose proof (getallchains_supports_x Y X Y' chainsY' YNil supYX) as supY'X.
+  pose proof (getallchains_supports_y Y' X X' chainsX' supY'X) as supY'X'.
+  clear supYX supY'X.
+  unfold supports in *. unfold supports_iso. split.
+  - intros H HInY'. pose proof (supX'Y' H HInY'). 
+    destruct H0 as [G H0]. destruct H0 as [GInX' GLeqH].
+    destruct GLeqH as [GEqH | GLeH].
+  -- exists G. auto.
+  -- pose proof (supY'X' G GInX'). destruct H0 as [F H0].
+    destruct H0 as [FInY' FLeqG].
+    destruct FLeqG as [FEqG | FLeG].
+  --- pose proof (po_trans_helper F G H).
+      assert (isomorphism F G /\ strict_partial_order G H) by auto.
+      apply H0 in H1. clear H0.
+      pose proof (getallchains_not_spo Y Y' chainsY' F FInY' H HInY'). contradiction.
+  --- assert (strict_partial_order F H).
+      { eapply spo_trans; eauto. }
+      pose proof (getallchains_not_spo Y Y' chainsY' F FInY' H HInY'). contradiction.
+  - intros H HInX'. pose proof (supY'X' H HInX').
+    destruct H0 as [G H0]. destruct H0 as [GInY' GLeqH].
+    destruct GLeqH as [GEqH | GLeH].
+  -- exists G. auto.
+  -- pose proof (supX'Y' G GInY'). destruct H0 as [F H0].
+     destruct H0 as [FInX' FLeqG].
+     destruct FLeqG as [FEqG | FLeG].
+  --- pose proof (po_trans_helper F G H).
+      assert (isomorphism F G /\ strict_partial_order G H) by auto.
+      apply H0 in H1. clear H0.
+      pose proof (getallchains_not_spo X X' chainsX' F FInX' H HInX'). contradiction.
+  --- assert (strict_partial_order F H).
+      { eapply spo_trans; eauto. }
+      pose proof (getallchains_not_spo X X' chainsX' F FInX' H HInX'). contradiction.
+Qed.
  
 
 End Supports_List.
